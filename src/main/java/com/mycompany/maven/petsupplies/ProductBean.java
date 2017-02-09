59bb97b44c1ea666201f3b734c6ebcbf7fe81359
flexi758 <serhildan.akdeniz@sogeti.com>
2017-02-09 16:25:31 +0100

    bootstrap + JPA

--- src/main/java/com/mycompany/maven/petsupplies/ProductBean.java
+++ src/main/java/com/mycompany/maven/petsupplies/ProductBean.java
@@ -6,15 +6,11 @@
 package com.mycompany.maven.petsupplies;
 
 import javax.ejb.Stateless;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.util.ArrayList;
 import java.util.List;
-
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 /**
  *
@@ -24,42 +20,11 @@ import javax.faces.bean.SessionScoped;
 @ManagedBean(name = "productBean")
 @SessionScoped
 public class ProductBean {
-
-    private static final String _URL = "jdbc:mysql://localhost:3306/petsuppliesdb";
-    private static final String _USER = "root";
-    private static final String _PASSWORD = "root";
+@PersistenceContext
+private EntityManager em;
 
     public List<Product> getProductList() {
-        List<Product> list = new ArrayList<Product>();
-        PreparedStatement ps = null;
-        Connection con = null;
-        ResultSet rs = null;
-        try {
-
-            Class.forName("com.mysql.jdbc.Driver");
-            con = DriverManager.getConnection(_URL, _USER, _PASSWORD);
-            String sql = "SELECT * FROM PRODUCT";
-            ps = con.prepareStatement(sql);
-            rs = ps.executeQuery();
-            while (rs.next()) {
-                Product prd = new Product();
-                prd.setId(rs.getInt("id"));
-                prd.setName(rs.getString("name"));
-                prd.setPrice(rs.getBigDecimal("price"));
-                prd.setDescription(rs.getString("description"));
-                list.add(prd);
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                con.close();
-                ps.close();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
+        List list = em.createNamedQuery("Product.findAll").getResultList();
         return list;
-    }
-
+        }
 }
